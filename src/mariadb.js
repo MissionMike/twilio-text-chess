f786c544798cc99fc1af78b3ecf640debedafd9b
Michael Dinerstein <md@missionmike.dev>
2020-11-29 05:43:54 +0000

    Added table structure comment

--- src/mariadb.js
+++ src/mariadb.js
@@ -1,3 +1,25 @@
+/**
+ * Database table should have the following structure:
+ * 
+    CREATE TABLE gamedata (
+        id varchar(15) NOT NULL,
+        city varchar(85),
+        state varchar(2),
+        country varchar(2),
+        fen varchar(90),
+        created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        primary key (id)
+    );
+ * 
+ * Set the appropriate environment variables in .env:
+ * 
+ * SQL_PASSWORD=
+ * SQL_USER=
+ * SQL_DATABASE=
+ * SQL_TABLE=
+ * SQL_HOST=
+ */
 const mariadb = require("mariadb");
 const table = process.env.SQL_DATABASE + "." + process.env.SQL_TABLE;
 const pool = mariadb.createPool({
